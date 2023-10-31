67f9e18c77f8f16e58f3a8933dc3cac9b8bdc984
Gri-ffin <gameshunter359@gmail.com>
2023-10-31 18:05:38 +0100

    update the button style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -71,7 +71,7 @@ function AuthButton() {
 
   return (
     <button
-      className="rounded-lg bg-blue-700/70 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+      className="rounded-xl full bg-blue-700/70 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
       onClick={sessionData ? () => void signOut() : () => void signIn()}
     >
       {sessionData ? "Sign out" : "Sign in"}
