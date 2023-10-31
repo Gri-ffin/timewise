8606b5b3399569aa676a513b863c26f933affbf1
Gri-ffin <gameshunter359@gmail.com>
2023-10-31 18:35:48 +0100

    protect route for the task dashboard

--- src/pages/dashboard/task.tsx
+++ src/pages/dashboard/task.tsx
@@ -1,9 +1,46 @@
+import type { GetServerSideProps } from "next"
+import { getServerSession } from "next-auth"
+import { useSession } from "next-auth/react"
+import { authOptions } from "~/server/auth"
+
+
+import { CgDanger } from 'react-icons/cg'
+
+import AuthButton from "~/components/AuthButton"
+
 const TaskPage = () => {
+  const { data: sessionData } = useSession()
+  console.log(sessionData);
+
+  if (sessionData !== null) {
+    return (
+      <div>
+        Task page
+      </div>
+    )
+  }
+
   return (
-    <div>
-      Task page
+    <div className="min-h-screen flex flex-col items-center justify-center space-y-7">
+      <CgDanger size={70} className="text-red-600/75" />
+      <h3 className="font-bold text-2xl">Access denied</h3>
+      <AuthButton />
     </div>
   )
+
 }
 
 export default TaskPage
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  return {
+    props: {
+      session: await getServerSession(
+        context.req,
+        context.res,
+        authOptions
+      )
+    }
+  }
+
+}
