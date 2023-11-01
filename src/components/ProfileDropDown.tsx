90195ccc93a2728f764afea5af4205878bfb06d8
Gri-ffin <gameshunter359@gmail.com>
2023-11-01 18:23:28 +0100

    finish the profile dropdown component

--- src/components/ProfileDropDown.tsx
+++ src/components/ProfileDropDown.tsx
@@ -1,8 +1,9 @@
-import { User } from "lucide-react"
+import { Calendar, Folder, Settings, User } from "lucide-react"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu"
 import { Button } from "./ui/button"
-import { BiTask } from "react-icons/bi"
+import { BiStats, BiTask } from "react-icons/bi"
 
+// TODO: change the spans to a links that redirect to the appropriate view
 const ProfileDropDown = ({ username }: { username: string }) => {
   return (
     <DropdownMenu>
@@ -19,7 +20,29 @@ const ProfileDropDown = ({ username }: { username: string }) => {
           </DropdownMenuItem>
           <DropdownMenuItem>
             <BiTask className="mr-2 w-4 h-4" />
-            <span>Task</span>
+            <span>Tasks</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <Folder className="mr-2 h-4 w-4" />
+            <span>Projects</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <Calendar className="mr-2 h-4 w-4" />
+            <span>Calendar</span>
+          </DropdownMenuItem>
+          <DropdownMenuItem>
+            <BiStats className="mr-2 h-4 w-4" />
+            <span>Stats</span>
+          </DropdownMenuItem>
+        </DropdownMenuGroup>
+        <DropdownMenuSeparator />
+        <DropdownMenuGroup>
+          <DropdownMenuItem>
+            <Settings className="mr-2 h-4 w-4" />
+            <span>Settings</span>
           </DropdownMenuItem>
         </DropdownMenuGroup>
       </DropdownMenuContent>
