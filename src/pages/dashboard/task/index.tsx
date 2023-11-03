845c480b387fb43dc1d02dba66c15f5a84c6554b
Gri-ffin <gameshunter359@gmail.com>
2023-11-03 18:36:57 +0100

    update the index page

--- src/pages/dashboard/task/index.tsx
+++ src/pages/dashboard/task/index.tsx
@@ -4,14 +4,14 @@ import { useSession } from "next-auth/react"
 import Link from 'next/link'
 
 import dayjs from 'dayjs'
-import { CgDanger } from 'react-icons/cg'
 
-import AuthButton from "~/components/AuthButton"
 import ProfileDropDown from "~/components/ProfileDropDown"
 import DateNavigationMenu from "~/components/task/DateNavigationMenu"
 import { authOptions } from "~/server/auth"
 import { Button } from "~/components/ui/button"
 import { TextIcon } from "lucide-react"
+import Head from "next/head"
+import AccessDenied from "~/components/AccessDenied"
 
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
@@ -24,40 +24,42 @@ const TaskPage = () => {
 
   if (sessionData !== null) {
     return (
-      <div className="min-h-screen">
-        <nav className="flex items-center justify-between">
-          <h1 className="font-extrabold text-lg">TimeWise</h1>
-          <ProfileDropDown username={sessionData.user.name!} />
-        </nav>
-        <main>
-          <div className="flex items-center justify-center my-5">
-            <DateNavigationMenu />
-          </div>
-          <section className="my-14">
-            <h3 className="text-3xl text-center">{currentDay}</h3>
-            <p className="text-slate-500 text-center my-4">{currentDate}</p>
-            <Link href='/dashboard/task/add'>
-              <Button className="w-full justify-start" variant='outline'>
-                <TextIcon className="mr-2 h-4 w-4" /> Add a task...
-              </Button>
-            </Link>
-            {/* TODO: Change this to the todos list once i start working on the db */}
-            <div className="flex flex-col items-center justify-center mt-44">
-              <p className="text-xl">Well, no tasks seems to be found.</p>
+      <>
+        <Head>
+          <title>TimeWise | Tasks Lists</title>
+          <meta name="description" content="Add tasks and view them based on day, week, month or year." />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <div className="min-h-screen">
+          <nav className="flex items-center justify-between">
+            <h1 className="font-extrabold text-lg">TimeWise</h1>
+            <ProfileDropDown username={sessionData.user.name!} imgUrl={sessionData.user.image!} />
+          </nav>
+          <main>
+            <div className="flex items-center justify-center my-5">
+              <DateNavigationMenu />
             </div>
-          </section>
-        </main>
-      </div>
+            <section className="my-14">
+              <h3 className="text-3xl text-center">{currentDay}</h3>
+              <p className="text-slate-500 text-center my-4">{currentDate}</p>
+              <Link href='/dashboard/task/add'>
+                <Button className="w-full justify-start" variant='outline'>
+                  <TextIcon className="mr-2 h-4 w-4" /> Add a task...
+                </Button>
+              </Link>
+              {/* TODO: Change this to the todos list once i start working on the db */}
+              <div className="flex flex-col items-center justify-center mt-44">
+                <p className="text-xl">Well, no tasks seems to be found.</p>
+              </div>
+            </section>
+          </main>
+        </div>
+      </>
     )
   }
 
-  return (
-    <div className="min-h-screen flex flex-col items-center justify-center space-y-7">
-      <CgDanger size={70} className="text-red-600/75" />
-      <h3 className="font-bold text-2xl">Access denied</h3>
-      <AuthButton />
-    </div>
-  )
+  return <AccessDenied />
+
 
 }
 
