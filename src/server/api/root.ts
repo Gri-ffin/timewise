c0cadce3487209d71ba0a28a114c870d3432aea8
Gri-ffin <gameshunter359@gmail.com>
2023-11-05 19:38:14 +0100

    rename variable from postRouter to taskRouter

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,4 +1,4 @@
-import { postRouter } from "~/server/api/routers/post";
+import { taskRouter } from "~/server/api/routers/task";
 import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
@@ -7,7 +7,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  post: postRouter,
+  task: taskRouter,
 });
 
 // export type definition of API
