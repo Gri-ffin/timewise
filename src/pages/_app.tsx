aae0da882d952fdff2be6e9847f36f35fcb0baec
Gri-ffin <gameshunter359@gmail.com>
2023-11-05 19:38:42 +0100

    remove unused import theme provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,7 +6,6 @@ import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 
 import "~/styles/globals.css";
-import { ThemeProvider } from '~/components/theme-provider';
 
 const inter = Inter({ subsets: ['latin'] })
 
