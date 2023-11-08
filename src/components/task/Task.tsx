0b6da999848d983f1eb3fe0de7754bb47f933435
Gri-ffin <gameshunter359@gmail.com>
2023-11-08 18:39:19 +0100

    invalidate the tasks to refresh the front

--- src/components/task/Task.tsx
+++ src/components/task/Task.tsx
@@ -2,33 +2,44 @@ import type { Task as TaskType } from "@prisma/client"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuTrigger } from "../ui/dropdown-menu"
 import { Button } from '~/components/ui/button'
 import { Trash } from "lucide-react"
+import { api } from "~/utils/api"
 
 const Task = ({ data }: { data: TaskType }) => {
-  // TODO: write the function to delete the task based on id
+  const deleteMutation = api.task.delete.useMutation()
+  const utils = api.useUtils()
+
   const deleteTask = () => {
-    console.log('Working');
+    deleteMutation.mutate({ id: data.id }, {
+      onSuccess: () => {
+        utils.task.invalidate()
+      }
+    })
 
   }
 
   return (
     <div className="py-1 w-full space-y-1">
-      <div className="flex flex-row items-center justify-between">
-        <h3 className="capitalize font-semibold">{data.title}</h3>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant='ghost'>. . .</Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-56 mx-5">
-            <DropdownMenuGroup>
-              <DropdownMenuItem>
-                <Trash className="w-4 h-4 mr-2" />
-                <span className="cursor-pointer" onClick={deleteTask}>Delete</span>
-              </DropdownMenuItem>
-            </DropdownMenuGroup>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      </div>
-      <p className="text-gray-500">{data.memo}</p>
+      {deleteMutation.isLoading ? <h3 className="font-semibold">Deleting . . .</h3> :
+        <>
+          <div className="flex flex-row items-center justify-between">
+            <h3 className="capitalize font-semibold">{data.title}</h3>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant='ghost'>. . .</Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-56 mx-5">
+                <DropdownMenuGroup>
+                  <DropdownMenuItem onClick={deleteTask} className="cursor-pointer">
+                    <Trash className="w-4 h-4 mr-2" />
+                    <span>Delete</span>
+                  </DropdownMenuItem>
+                </DropdownMenuGroup>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </div>
+          <p className="text-gray-500">{data.memo}</p>
+        </>
+      }
     </div>
   )
 }
