c41846a1b8ac2331051d23837f8eed4a8d7a114b
Gri-ffin <gameshunter359@gmail.com>
2023-11-09 18:41:45 +0100

    switch between the period of tasks to show

--- src/components/task/DateNavigationMenu.tsx
+++ src/components/task/DateNavigationMenu.tsx
@@ -1,24 +1,30 @@
 import Link from 'next/link'
-import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, navigationMenuTriggerStyle } from "../ui/navigation-menu"
 
-const navigationItems = [
-  { name: 'Day', path: '/' },
-  { name: 'Week', path: '/week' },
-  { name: 'Month', path: '/month' },
-  { name: 'Year', path: '/year' },
+import { NavigationMenu, NavigationMenuItem, NavigationMenuList } from "../ui/navigation-menu"
+import { Button } from '~/components/ui/button'
+import type { Period } from '~/utils/task/types'
+
+interface NavigationItem {
+  name: string,
+  period: Period
+}
+
+const navigationItems: NavigationItem[] = [
+  { name: 'Day', period: 'day' },
+  { name: 'Week', period: 'week' },
+  { name: 'Month', period: 'month' },
+  { name: 'Year', period: 'year' },
 ]
 
-const DateNavigationMenu = () => {
+const DateNavigationMenu = ({ changePeriod, period }: { changePeriod: (period: Period) => void, period: Period }) => {
   return (
     <NavigationMenu>
       <NavigationMenuList>
         {navigationItems.map(navigationItem => (
-          <NavigationMenuItem key={navigationItem.path}>
-            <Link href={`/dashboard${navigationItem.path}`} legacyBehavior passHref>
-              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                {navigationItem.name}
-              </NavigationMenuLink>
-            </Link>
+          <NavigationMenuItem key={navigationItem.period} onClick={() => changePeriod(navigationItem.period)}>
+            <Button className={`${navigationItem.period === period && 'bg-gray-600 text-white hover:text-black'}`}>
+              {navigationItem.name}
+            </Button>
           </NavigationMenuItem>
         ))}
       </NavigationMenuList>
