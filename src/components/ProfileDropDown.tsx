4c1279dbb56022efb3ef7b5e0a2e71c10abef267
Gri-ffin <gameshunter359@gmail.com>
2023-11-11 19:21:50 +0100

    update the profile drop down

--- src/components/ProfileDropDown.tsx
+++ src/components/ProfileDropDown.tsx
@@ -1,8 +1,10 @@
-import { Calendar, Folder, LogOut, Settings, User } from "lucide-react"
-import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu"
-import { BiStats, BiTask } from "react-icons/bi"
-import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
+import { Calendar, Folder, LogOut, Plus, Settings, User } from "lucide-react"
+import { BiFile, BiStats, BiTask } from "react-icons/bi"
 import { signOut } from "next-auth/react"
+import Link from 'next/link'
+
+import { DropdownMenu, DropdownMenuContent, DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuPortal, DropdownMenuSeparator, DropdownMenuSub, DropdownMenuSubContent, DropdownMenuSubTrigger, DropdownMenuTrigger } from "./ui/dropdown-menu"
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar"
 
 // TODO: change the spans to a links that redirect to the appropriate view
 // this is the menu that the user can choose to quickly navigate
@@ -19,14 +21,35 @@ const ProfileDropDown = ({ username, imgUrl }: { username: string, imgUrl: strin
         <DropdownMenuLabel>My account</DropdownMenuLabel>
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
+          {/* Profile Section */}
           <DropdownMenuItem>
             <User className="mr-2 h-4 w-4" />
             <span>Profile</span>
           </DropdownMenuItem>
-          <DropdownMenuItem>
-            <BiTask className="mr-2 w-4 h-4" />
-            <span>Tasks</span>
-          </DropdownMenuItem>
+          {/* Task Section */}
+          <DropdownMenuSub>
+            <DropdownMenuSubTrigger>
+              <BiTask className="mr-2 h-4 w-4" />
+              <span>Tasks</span>
+            </DropdownMenuSubTrigger>
+            <DropdownMenuPortal>
+              <DropdownMenuSubContent>
+                <Link href='/dashboard/task/add' >
+                  <DropdownMenuItem className="flex items-center cursor-pointer">
+                    <Plus className="mr-2 h-4 w-4" />
+                    <span>Add</span>
+                  </DropdownMenuItem>
+                </Link>
+                <Link href='/dashboard/task'>
+                  <DropdownMenuItem className="flex items-center cursor-pointer">
+                    <BiFile className="mr-2 h-4 w-4" />
+                    <span>List</span>
+                  </DropdownMenuItem>
+                </Link>
+              </DropdownMenuSubContent>
+            </DropdownMenuPortal>
+          </DropdownMenuSub>
+          {/* Projects Section */}
           <DropdownMenuItem>
             <Folder className="mr-2 h-4 w-4" />
             <span>Projects</span>
@@ -34,10 +57,12 @@ const ProfileDropDown = ({ username, imgUrl }: { username: string, imgUrl: strin
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
+          {/* Calendar Section */}
           <DropdownMenuItem>
             <Calendar className="mr-2 h-4 w-4" />
             <span>Calendar</span>
           </DropdownMenuItem>
+          {/* Stats Section */}
           <DropdownMenuItem>
             <BiStats className="mr-2 h-4 w-4" />
             <span>Stats</span>
@@ -45,10 +70,12 @@ const ProfileDropDown = ({ username, imgUrl }: { username: string, imgUrl: strin
         </DropdownMenuGroup>
         <DropdownMenuSeparator />
         <DropdownMenuGroup>
+          {/* Settings Section */}
           <DropdownMenuItem>
             <Settings className="mr-2 h-4 w-4" />
             <span>Settings</span>
           </DropdownMenuItem>
+          {/* Sign out section */}
           <DropdownMenuItem onClick={() => signOut({ callbackUrl: "/" })}>
             <LogOut className="mr-2 w-4 h-4" />
             <span>Sign out</span>
