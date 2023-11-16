f7b758155f5f3ed450a5b0a86d4a716ac97153c7
Gri-ffin <gameshunter359@gmail.com>
2023-11-16 20:27:14 +0100

    add function to get group icon

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,4 +1,5 @@
 import { BsGithub, BsQuestionLg } from 'react-icons/bs'
+import { Folder, Star, File, Flag, Power, X } from 'lucide-react'
 
 export const getIcon = (type: string) => {
   switch (type) {
@@ -7,5 +8,22 @@ export const getIcon = (type: string) => {
 
     default:
       return BsQuestionLg;
+  }
+}
+
+export const getGroupIcon = (iconName: string) => {
+  switch (iconName) {
+    case 'Folder':
+      return Folder
+    case 'Star':
+      return Star
+    case 'File':
+      return File
+    case 'Flag':
+      return Flag
+    case 'Power':
+      return Power
+    default:
+      return X
   }
 }
