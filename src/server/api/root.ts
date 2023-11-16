cfdfb32334b3345306e08b406ceecfd13802bf83
Gri-ffin <gameshunter359@gmail.com>
2023-11-16 18:44:57 +0100

    add group to routers root

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,6 @@
 import { taskRouter } from "~/server/api/routers/task";
 import { createTRPCRouter } from "~/server/api/trpc";
+import { groupRouter } from "./routers/group";
 
 /**
  * This is the primary router for your server.
@@ -8,6 +9,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
  */
 export const appRouter = createTRPCRouter({
   task: taskRouter,
+  group: groupRouter
 });
 
 // export type definition of API
---
+++ src/server/api/routers/group.ts
@@ -0,0 +1,26 @@
+import * as z from 'zod'
+
+import {
+  createTRPCRouter,
+  protectedProcedure,
+} from "~/server/api/trpc";
+import { ICONSTYPES } from '~/utils/group/types';
+
+export const groupRouter = createTRPCRouter({
+  // the precedure the create the group 
+  create: protectedProcedure
+    .input(z.object(
+      {
+        name: z.string().min(3).max(10),
+        icon: z.enum(ICONSTYPES)
+      }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.group.create({
+        data: {
+          name: input.name,
+          icon: input.icon,
+          user: { connect: { id: ctx.session.user.id } },
+        },
+      });
+    }),
+})
