639000ee3de04c9b346bee481a669101e888b8d6
Gri-ffin <gameshunter359@gmail.com>
2023-11-16 20:29:13 +0100

    remove font weight

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,7 @@ import ProfileDropDown from "./ProfileDropDown"
 const Navbar = ({ username, imgUrl }: { username: string, imgUrl: string }) => {
   return (
     <nav className="flex items-center justify-between">
-      <h1 className="font-extrabold text-lg">TimeWise</h1>
+      <h1 className="text-xl">TimeWise</h1>
       <ProfileDropDown username={username} imgUrl={imgUrl} />
     </nav>
   )
