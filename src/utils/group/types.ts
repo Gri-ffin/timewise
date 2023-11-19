4d860493bc6a650ba7fbeef5ea53e6603797c5ad
Gri-ffin <gameshunter359@gmail.com>
2023-11-19 19:28:28 +0100

    change accepted length of name

--- src/utils/group/types.ts
+++ src/utils/group/types.ts
@@ -6,8 +6,8 @@ export const groupTaskSchema = z.object({
   name: z.string()
     .min(3,
       { message: 'The title must be at least 3 chars.' })
-    .max(10,
-      { message: 'The title must be at most 10 chars.' }),
+    .max(20,
+      { message: 'The title must be at most 20 chars.' }),
   icon: z.enum(ICONSTYPES)
 })
 
