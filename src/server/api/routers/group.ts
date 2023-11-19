ba75bc7ba21ef7c6348aa5c539ead621bca92c97
Gri-ffin <gameshunter359@gmail.com>
2023-11-19 19:28:41 +0100

    add the update and delete functionalities

--- src/server/api/routers/group.ts
+++ src/server/api/routers/group.ts
@@ -12,7 +12,7 @@ export const groupRouter = createTRPCRouter({
   create: protectedProcedure
     .input(z.object(
       {
-        name: z.string().min(3).max(10),
+        name: z.string().min(3).max(20),
         icon: z.enum(ICONSTYPES)
       }))
     .mutation(async ({ ctx, input }) => {
@@ -42,6 +42,38 @@ export const groupRouter = createTRPCRouter({
           Task: true
         }
       })
+    }),
+  // the procedure the update the task based on the id
+  update: protectedProcedure
+    .input(z.object({
+      id: z.number(),
+      name: z.string().min(3).max(20),
+      icon: z.enum(ICONSTYPES),
+    }))
+    .mutation(async ({ ctx, input }) => {
+      {
+        return ctx.db.group.update({
+          where: {
+            user: { id: ctx.session.user.id },
+            id: input.id,
+          },
+          data: {
+            name: input.name,
+            icon: input.icon,
+          }
+        });
+      }
+    }),
+  delete: protectedProcedure
+    .input(z.object({
+      id: z.number(),
+    }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.group.delete({
+        where: {
+          user: { id: ctx.session.user.id },
+          id: input.id,
+        },
+      });
     })
-
 })
